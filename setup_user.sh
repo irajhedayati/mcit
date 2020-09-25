b8de39b0c093731b42f79bbe6cdbe20ac2ecc75b
Iraj Hedayati <iraj.hedayati@gmail.com>
2020-09-25 10:10:07 -0400

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-USER_NAME=$1
-sudo -u hdfs hadoop fs -mkdir /user/$USER_NAME
-sudo -u hdfs hadoop fs -chown -R $USER_NAME:supergroup /user/$USER_NAME
+GROUP=$1
+USER_NAME=$2
+
+sudo -u hdfs hadoop fs -mkdir /user/$GROUP/$USER_NAME
+sudo -u hdfs hadoop fs -chown -R $USER_NAME:supergroup /user/$GROUP/$USER_NAME
