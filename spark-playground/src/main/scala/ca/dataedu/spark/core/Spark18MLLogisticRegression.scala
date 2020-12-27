964cf53ac23e0155684f6aa8be419fffc3ac57b0
Iraj Hedayati <iraj.hedayati@paytm.com>
2020-12-27 13:35:06 -0500

    Change LogisticRegression dataset

--- spark-playground/src/main/scala/ca/dataedu/spark/core/Spark18MLLogisticRegression.scala
+++ spark-playground/src/main/scala/ca/dataedu/spark/core/Spark18MLLogisticRegression.scala
@@ -2,48 +2,65 @@ package ca.dataedu.spark.core
 
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object Spark18MLLogisticRegression extends App with Base {
 
   val spark = SparkSession.builder().appName("Spark ML").master("local[*]").getOrCreate()
   /*
-  Labeled data (7 entries): we divide it in two set
-  =================================
-  1. Training dataset (4 entries) to create the model
-  0.0  1.1 0.1       => 1
-  2.0  1.0 -1.0      => 0
-  2.0  1.3 1.0       => 0
-  0.0  1.2 -0.5      => 1
-
-  2. Test dataset (3 entries) to evaluate the model
-  -1.0 1.5 1.3
-  3.0  2.0 -0.1
-  0.0  2.2 -1.5
+  Input data is labeled
+  Feature columns: Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age
+  Label: Outcome
   */
 
-  val training: DataFrame = spark.createDataFrame(Seq(
-    (1.0, Vectors.dense(0.0, 1.1, 0.1)),
-    (0.0, Vectors.dense(2.0, 1.0, -1.0)),
-    (0.0, Vectors.dense(2.0, 1.3, 1.0)),
-    (1.0, Vectors.dense(0.0, 1.2, -0.5))
-  )).toDF("label", "features")
+  import spark.implicits._
 
-  val lr = new LogisticRegression()
-  val model = lr.fit(training)
+  val schema = StructType(List(
+    StructField("Pregnancies", DoubleType),
+    StructField("Glucose", DoubleType),
+    StructField("BloodPressure", DoubleType),
+    StructField("SkinThickness", DoubleType),
+    StructField("Insulin", DoubleType),
+    StructField("BMI", DoubleType),
+    StructField("DiabetesPedigreeFunction", DoubleType),
+    StructField("Age", DoubleType),
+    StructField("Outcome", DoubleType)
+  ))
 
-  val testData = spark.createDataFrame(Seq(
-    (1.0, Vectors.dense(-1.0, 1.5, 1.3)),
-    (0.0, Vectors.dense(3.0, 2.0, -0.1)),
-    (1.0, Vectors.dense(0.0, 2.2, -1.5))
-  )).toDF("label", "features")
-
-  model.transform(testData)
-    .select("features", "label", "probability", "prediction")
-    .collect()
-    .foreach { case Row(features, label: Double, prob, prediction: Double) =>
-      println(s"($features, $label) -> prob=$prob, prediction=$prediction")
+  val input: DataFrame = spark.read.option("header", "true")
+    .schema(schema)
+    // Read training data
+    .csv("/Users/iraj.hedayati/codes/mcit/spark-playground/data/diabetes.csv")
+    // Select features and labels
+    .select("Pregnancies", "Insulin", "BMI", "Age", "Glucose", "BloodPressure", "DiabetesPedigreeFunction", "Outcome")
+    .rdd
+    .map { row =>
+      val featuresVector =
+        Vectors.dense(
+          row.getDouble(0),
+          row.getDouble(1),
+          row.getDouble(2),
+          row.getDouble(3),
+          row.getDouble(4),
+          row.getDouble(5),
+          row.getDouble(6)
+        )
+      row.getDouble(7) -> featuresVector
     }
+    .toDF("label", "features")
+
+  input.randomSplit(Array(0.8, 0.2)) match {
+    case Array(trainingDataset, testDataset) =>
+      val lr = new LogisticRegression()
+      val model = lr.fit(trainingDataset)
+      model.transform(testDataset)
+        .select("features", "label", "probability", "prediction")
+        .collect()
+        .foreach { case Row(features, label: Double, prob, prediction: Double) =>
+          println(s"($features, $label) -> prob=$prob, prediction=$prediction")
+        }
+  }
 
   spark.stop()
 
