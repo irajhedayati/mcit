35f6184ad821dc91c66b03e9f2b405adc7e35eb8
Iraj Hedayati <iraj.hedayati@gmail.com>
2021-04-13 22:56:09 -0400

    Update Spark project

--- spark-playground/src/main/scala/ca/dataedu/spark/core/Spark03Sql.scala
+++ spark-playground/src/main/scala/ca/dataedu/spark/core/Spark03Sql.scala
@@ -7,43 +7,52 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object Spark03Sql extends App with Base {
 
-  val spark = SparkSession.builder()
-    .appName("Spark SQL practice").master("local[*]")
-    .getOrCreate()
+  // 0. create spark session (it contains a spark context)
+  val spark = SparkSession.builder().appName("Spark SQL Practices").master("local[*]").getOrCreate()
+
+  // 1. how to get the spark context; to create RDD of String
+  val rawMovie: RDD[String] = spark.sparkContext.parallelize(
+    List(
+      "101,Gone with the Wind,1939,Victor Fleming",
+      "102,Star Wars,1977,George Lucas"
+    )
+  )
+
+  // 2. how ot create DF from RDD using inference
+  // 2.a create RDD of "case class" (e.g. RDD[Movie])
+  // 2.b use `.toDF` from Spark session implicits
+  val movieRdd: RDD[Movie] = rawMovie.map(Movie(_))
   import spark.implicits._
+  val movieDf: DataFrame = movieRdd.toDF
 
-  val x = spark.read.parquet("file:///Users/iraj.hedayati/Downloads/1_0_00000000003186881615")
-  x.printSchema()
-  x.show(10, truncate = false)
+  // THIS IS FOR DEBUG AND SHALL NOT BE IN ANY PRODUCTION CODE NOR IN THE SUBMISSION
+  movieDf.printSchema()
+  movieDf.show() // shows ... records and if the values are big they will be truncated
+  movieDf.show(20) // shows 20 records and if the values are big they will be truncated
+  movieDf.show(20, truncate = false) // shows 20 records and does not truncate
 
-//  val movieRdd: RDD[String] = spark.sparkContext.parallelize(List(
-//    "101,Gone with the Wind,1939,Victor Fleming",
-//    "102,Star Wars,1977,George Lucas"
-//  ))
-//  // Inferring schema using reflection
-//  val movieRdd2: RDD[Movie] = movieRdd
-//    .map(_.split(","))
-//    .map(csv => new Movie(csv(0).toInt, csv(1), csv(2).toInt, Some(csv(3))))
-//  val movieDf: DataFrame = movieRdd2.toDF
-//
-//  movieDf.show()
-//  movieDf.printSchema()
-//
-//  // programmatic interface
-//  val movieRdd3: RDD[Row] = movieRdd
-//    .map(_.split(","))
-//    .map(csv => Row(csv(0).toInt, csv(1), csv(2).toInt, csv(3)))
-//  val schema: StructType = StructType(List(
-//    StructField("mID", IntegerType, nullable = false),
-//    StructField("title", StringType, nullable = false),
-//    StructField("year", IntegerType),
-//    StructField("director", StringType)
-//  ))
-//  val movieDf2: DataFrame = spark.createDataFrame(movieRdd3, schema)
-//
-//  movieDf2.show()
-//  movieDf2.printSchema()
+  // 3. how to create DF from RDD using programming interface
+  // 3.a convert it to RDD[Row]
+  // 3.b create a schema
+  // 3.c apply schema on the RDD
+  val movieRows: RDD[Row] = rawMovie
+    .map(_.split(",", -1))
+    .map(fields => Row(fields(0).toInt, fields(1), fields(2).toInt, fields(3)))
+  val schema = StructType(
+    List(
+      StructField("mId", IntegerType, nullable  = false),
+      StructField("title", StringType, nullable = false),
+      StructField("year", IntegerType, nullable = true),
+      StructField("director", StringType)
+    )
+  )
+  val movieDf2: DataFrame = spark.createDataFrame(movieRows, schema)
 
+  // THIS IS FOR DEBUG AND SHALL NOT BE IN ANY PRODUCTION CODE NOR IN THE SUBMISSION
+  movieDf2.printSchema()
+  movieDf2.show() // shows ... records and if the values are big they will be truncated
+
+  // 100. stop spark session (which also stoops spark context)
   spark.stop()
 
 }
--- spark-playground/src/main/scala/ca/dataedu/spark/core/Spark04SqlFromSource.scala
+++ spark-playground/src/main/scala/ca/dataedu/spark/core/Spark04SqlFromSource.scala
@@ -10,6 +10,9 @@ object Spark04SqlFromSource extends App with Base {
   val spark = SparkSession.builder().appName("Spark SQL practice").master("local[*]").getOrCreate()
 
   val movieDf: DataFrame = spark.read.option("header", "true").option("inferschema", "true").csv(fileName)
+  // THIS IS FOR DEBUG AND SHALL NOT BE IN ANY PRODUCTION CODE NOR IN THE SUBMISSION
+  movieDf.printSchema()
+  movieDf.show()
 
   spark.stop()
 }
