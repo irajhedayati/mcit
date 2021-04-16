7dfa9222aae3258dc3de72e9491e159a53157ccf
Iraj Hedayati <iraj.hedayati@gmail.com>
2021-04-15 21:55:21 -0400

    Update Spark project

--- spark-playground/src/main/scala/ca/dataedu/spark/core/Spark11StreamingWithSql.scala
+++ spark-playground/src/main/scala/ca/dataedu/spark/core/Spark11StreamingWithSql.scala
@@ -7,37 +7,38 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object Spark11StreamingWithSql extends App with Base {
 
-  // 0. Spark conf
-  val spark = SparkSession.builder()
-    .appName("Spark SQL practice").master("local[*]")
-    .getOrCreate()
+  /**
+    * Problem:
+    * - I have all the movies on HDFS.
+    * - I get rating information regularly through FTP on the HDFS in CSV format
+    * - Once I received the rating file(s), I'd like to calculate the average rating
+    *
+    * Limitation: the average is calculated per batch and it is not a global average
+    */
+  val spark = SparkSession.builder().appName("Spark SQL Practices").master("local[*]").getOrCreate()
   val ssc = new StreamingContext(spark.sparkContext, Seconds(10))
 
-  // 1. read dimension into dataframe
-  /** on HDFS */
-  val fileName = "/user/fall2019/iraj/movie/movie.csv"
-  val movieDf: DataFrame = spark.read.option("header", "true").option("inferschema", "true").csv(fileName)
+  val movieDf = spark.read
+    .option("header", "true")
+    .option("inferschema", "true")
+    .csv("/user/bdsf2001/iraj/movie/movie.csv")
 
-  // 2. Create the stream (DStream)
-  val fileStream: DStream[String] = ssc.textFileStream("/user/fall2019/iraj/stream_input/")
+  val ratingStream = ssc.textFileStream("/user/bdsf2001/iraj/rating/")
 
-  // 3. Business logic
-  // networkSocketStream.print()
-  fileStream.foreachRDD{ rdd =>
+  ratingStream.foreachRDD { ratingRdd =>
     import spark.implicits._
-    val ratingDf = rdd.map(Rating(_)).toDF()
+    val ratingDf = ratingRdd.filter(!_.contains("rID")).map(Rating(_)).toDF()
     movieDf.createOrReplaceTempView("movie")
     ratingDf.createOrReplaceTempView("rating")
-    val joinResult = spark.sql(
+    val enrichedRating1 = spark.sql(
       """SELECT movie.title, avg(rating.stars) AS rating
         |FROM movie JOIN rating ON movie.mID = rating.mID
         |GROUP BY movie.title
-        |""".stripMargin)
-    joinResult.coalesce(1).write.mode(SaveMode.Append).json("/user/fall2019/iraj/movie_rating_json2")
-
+        |""".stripMargin
+    )
+    enrichedRating1.show()
   }
 
-  // 4. Start the streaming and keep it running
   ssc.start()
   ssc.awaitTermination()
   ssc.stop(stopSparkContext = true, stopGracefully = true)
